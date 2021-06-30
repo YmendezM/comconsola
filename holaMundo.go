f1eb2156aa14d7564b11deed1618c258e9bb16e3
YmendezM <ysmendezm@gmail.com>
2021-06-30 10:16:02 -0400

    Manejo de funciones

--- holaMundo.go
+++ holaMundo.go
@@ -20,6 +20,17 @@ func main() {
 		precio: 100,
 		plana: true,
 	}
+	var gorra2 = Gorra {
+		"Adidas",
+		"negra",
+		100,
+		true,
+	}
+	
+	holaMundo(gorra1, gorra2)
+}
+
+func holaMundo(gorra1 Gorra, gorra2 Gorra){
 
 	fmt.Println(gorra1)
 	var suma int = 9 + 9 //decalracion de variables
@@ -32,13 +43,6 @@ func main() {
 	const numero int = 14
 	fmt.Println(valorInferido + numero) //opera
 	fmt.Println(valorInferido, numero) // concatena
-
-	var gorra2 = Gorra {
-		"Adidas",
-		"negra",
-		100,
-		true,
-	}
 
 	fmt.Println(gorra2.marca)
 
