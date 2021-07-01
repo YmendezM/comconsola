490784c29897eb4e129b61223ee6efad12dde650
YmendezM <ysmendezm@gmail.com>
2021-07-01 10:02:54 -0400

    Iterador switch

--- iteradores.go
+++ iteradores.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main()  {
 
@@ -22,6 +25,26 @@ func main()  {
 
 	for _, arr :=  range array{
 		fmt.Println(arr)
+	}
+
+	momento := time.Now()
+	hoy := momento.Weekday()
+	switch hoy {
+	case 0 :
+		fmt.Println("Hoy es DOMINGO")
+	case 1 :
+		fmt.Println("Hoy es LUNES")
+	case 2 :
+		fmt.Println("Hoy es MARTES")
+	case 3 :
+		fmt.Println("Hoy es MIERCOLES")
+	case 4 :
+		fmt.Println("Hoy es JUEVES")
+	case 5 :
+		fmt.Println("Hoy es VIERNES")
+	default:
+		fmt.Println("Hoy es SABADO")
+
 	}
 
 
