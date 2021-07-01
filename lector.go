391226ee5f5289656647ba314acaa86595f7234c
YmendezM <ysmendezm@gmail.com>
2021-07-01 11:41:17 -0400

    Escritura de ficheros

--- lector.go
+++ lector.go
@@ -3,13 +3,26 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 func main()  {
  fmt.Println("Lector")
+
+ nuevoTexto := os.Args[1]+"\n"
+ //escribir := ioutil.WriteFile("fichero.txt", nuevoTexto, 0777)
+ fichero, err := os.OpenFile("fichero.txt", os.O_RDWR, 0777)
+ showError(err)
+ escribir, err := fichero.WriteString(nuevoTexto)
+ fmt.Println(escribir)
+ showError(err)
+ fichero.Close()
+
+
  texto, errorFichero := ioutil.ReadFile("fichero.txt")
  showError(errorFichero)
  fmt.Println(string(texto))
+
 
 }
 
