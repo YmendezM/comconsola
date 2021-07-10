56d387478a143f581985f8cde8f525a838ae0061
YmendezM <ysmendezm@gmail.com>
2021-07-10 02:38:27 -0400

    Manejo de rutas

--- goRest/main.go
+++ goRest/main.go
@@ -2,24 +2,32 @@ package main
 
 import (
 	"fmt"
-	//"github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 func main() {
-	addr := ":8080"
-    http.HandleFunc("/index", MyHandler)
+	/*addr := ":8080"
+    http.HandleFunc("/", MyHandler)
     log.Println("listen on", addr)
-    log.Fatal( http.ListenAndServe(addr, nil) )
+    log.Fatal( http.ListenAndServe(addr, nil) )*/
 
-	/*r := mux.NewRouter().StrictSlash(true)
+	r := mux.NewRouter().StrictSlash(true)
 	r.HandleFunc("/", Index)
+	r.HandleFunc("/contacto", Contacto)
+	r.HandleFunc("/addServes", Index)
 
-	server := http.ListenAndServe("localhost:8080", r)
-	log.Fatal(server)*/
+	addr := ":8080"
+	log.Println("listen on", addr)
+	server := http.ListenAndServe(addr, r)
+	log.Fatal(server)
 }
 
-func MyHandler(w http.ResponseWriter, r *http.Request) {
+func Index(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hola Mundo server Go")
+}
+
+func Contacto(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Ysrael Mendez Marciales.")
 }
