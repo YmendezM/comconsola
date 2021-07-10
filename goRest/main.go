2e33e080a319c814b509aaadc030c83dfc83962a
YmendezM <ysmendezm@gmail.com>
2021-07-10 03:06:30 -0400

    Parametros por rutas

--- goRest/main.go
+++ goRest/main.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	/*addr := ":8080"
-    http.HandleFunc("/", MyHandler)
-    log.Println("listen on", addr)
-    log.Fatal( http.ListenAndServe(addr, nil) )*/
+	  http.HandleFunc("/", MyHandler)
+	  log.Println("listen on", addr)
+	  log.Fatal( http.ListenAndServe(addr, nil) )*/
 
 	r := mux.NewRouter().StrictSlash(true)
 	r.HandleFunc("/", Index)
 	r.HandleFunc("/contacto", Contacto)
-	r.HandleFunc("/addServes", Index)
+	r.HandleFunc("/peliculas", peliculasList)
+	r.HandleFunc("/peliculas/{id}", peliculasShow)
 
 	addr := ":8080"
 	log.Println("listen on", addr)
@@ -25,9 +27,20 @@ func main() {
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hola Mundo server Go")
+	fmt.Fprintf(w, "Hola Mundo server Go")
 }
 
 func Contacto(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Ysrael Mendez Marciales.")
+}
+
+func peliculasList(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Listado de peliculas")
+}
+
+func peliculasShow(w http.ResponseWriter, r *http.Request) {
+
+	params := mux.Vars(r)
+	pelicula_id := params["id"]
+	fmt.Fprintf(w, "Cargando pelicula numero %s", pelicula_id)
 }
